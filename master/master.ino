d64ae3b0ae59a08ca053dad4ff06c11dff484ccb
mkpas <mpas0201@uni.sydney.edu.au>
2017-06-16 19:15:32 +1000

    Add files via upload

---
+++ master/master.ino
@@ -0,0 +1,111 @@
+String retSymb = "+RTINQ=";                     // start symble when there's any return
+String slaveName = ";SlaveUnique";                // caution that ';'must be included, and make sure the slave name is right.
+
+
+int nameIndex = 0;
+int addrIndex = 0;
+
+String recvBuf;
+String slaveAddr;
+
+
+String connectCmd = "\r\n+CONN=";
+
+void setup() {
+  Serial.begin(9600);
+  setupBlueToothConnection();
+  delay(1000);
+  Serial.flush();
+  Serial2.flush();
+}
+
+void loop() {
+   char recvChar;
+    while(1)
+    {
+        if(Serial.available())
+        {//check if there's any data sent from the local serial terminal, you can add the other applications here
+          recvChar  = Serial.read();
+          Serial2.write(recvChar);
+          Serial.println(recvChar);
+        }
+
+        if (Serial2.available()) {       //check if there's any data sent from the remote bluetooth shield
+          recvChar = Serial2.read();
+          Serial.print(recvChar);
+        }
+    }
+}
+
+void setupBlueToothConnection()
+{
+    Serial2.begin(38400);                           // Set BluetoothBee BaudRate to default baud rate 38400
+    Serial2.print("\r\n+STWMOD=1\r\n");             // set the bluetooth work in master mode
+    Serial2.print("\r\n+STNA=MasterUnique\r\n");    // set the bluetooth name as "SomethingUnique"
+    Serial2.print("\r\n+STAUTO=0\r\n");             // Auto-connection should be forbidden here
+    delay(2000);                                            // This delay is required.
+    Serial2.print("\r\n+INQ=1\r\n");                // make the master bluetooth inquirable
+    Serial.println("The master bluetooth is inquiring!");
+    delay(2000);                                            // This delay is required.
+    Serial2.flush();
+     //find the target slave
+    char recvChar;
+    while(1)
+    {
+        if(Serial2.available())
+        {
+            recvChar = Serial2.read();
+            recvBuf += recvChar;
+            nameIndex = recvBuf.indexOf(slaveName);             //get the position of slave name
+          
+                                                                //nameIndex -= 1;
+                                                                //decrease the ';' in front of the slave name, to get the position of the end of the slave address
+            if ( nameIndex != -1 )
+            {
+                //Serial.print(recvBuf);
+                addrIndex = (recvBuf.indexOf(retSymb,(nameIndex - retSymb.length()- 18) ) + retSymb.length());//get the start position of slave address
+                slaveAddr = recvBuf.substring(addrIndex, nameIndex);//get the string of slave address
+                break;
+            }
+        }
+    }
+    
+    //form the full connection command
+    connectCmd += slaveAddr;
+    connectCmd += "\r\n";
+    int connectOK = 0;
+    Serial.print("Connecting to slave:");
+    Serial.print(slaveAddr);
+    Serial.println(slaveName);
+    //connecting the slave till they are connected
+    do
+    {
+        Serial2.print(connectCmd);//send connection command
+        recvBuf = "";
+        while(1)
+        {
+            if(Serial2.available()){
+                recvChar = Serial2.read();
+                recvBuf += recvChar;
+                if(recvBuf.indexOf("CONNECT:OK") != -1)
+                {
+                    connectOK = 1;
+                    Serial.println("Connected!");
+                    Serial2.print("Connected!");
+                    break;
+                }
+                else if(recvBuf.indexOf("CONNECT:FAIL") != -1)
+                {
+                    Serial.println("Connect again!");
+                    break;
+                }
+            }
+        }
+    }while(0 == connectOK);
+
+  Serial2.flush();
+  delay(500);
+  while(Serial2.available()) {
+    Serial.write(Serial2.read());
+  }
+}
---
+++ slave/slave.ino
@@ -0,0 +1,290 @@
+#include <Servo.h>            // Include servo library
+#include <SoftwareSerial.h>   //Software Serial Port
+
+
+#define RxD 7
+#define TxD 6
+
+SoftwareSerial blueToothSerial(RxD, TxD);
+
+Servo left;                             // Declare left servo signal
+Servo right;                            // Declare right servo signal
+const int speedRight = 1450;            // Range: 1500(Stop)~1000(Full speed)
+const int speedLeft = 1550;             // Range: 1500(Stop)~2000(Full speed)
+const int revRight = 1550;              // Range: 1500(Stop)~2000(Full speed)
+const int revLeft = 1450;               // Range: 1500(Stop)~1000(Full speed)
+const int servoStop = 1500;
+const int rightBound = 600;
+const int leftBound = 700;
+const int rightWall = 600;
+const int leftWall = 600;
+
+int sensor1 = A0; //connected to analog 0 (PHOTOTRANSISTOR READING)
+int sensor2 = A2; //connected to analog 2 (PHOTOTRANSISTOR READING)
+int sensor3 = A3; //connected to analog 3 (PHOTOTRANSISTOR READING)
+int sensor4 = A4; //connected to analog 4 (PHOTOTRANSISTOR READING)
+int ledPin = 9;    // LED connected to digital pin 9 (IR LED PIN)
+
+boolean autopilot = false;
+char recvChar;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(RxD, INPUT);
+  pinMode(TxD, OUTPUT);
+  left.attach(13);
+  right.attach(12);
+  setupBlueToothConnection();
+  delay(1000);
+  Serial.flush();
+}
+
+void loop() {
+  while (1) {
+    if (blueToothSerial.available()) {       //check if there's any data sent from the remote bluetooth shield
+      recvChar = blueToothSerial.read();
+      Serial.print(recvChar);
+
+      switch (recvChar) {
+        case 'w':
+          goForward();
+          blueToothSerial.println("Moving forward.");
+          break;
+        case 'a':
+          goLeft();
+          blueToothSerial.println("Moving left.");
+          break;
+        case 's':
+          reverse();
+          blueToothSerial.println("Reversing.");
+          break;
+        case 'd':
+          goRight();
+          blueToothSerial.println("Moving right.");
+          break;
+        case 'q':
+          turnOnSpotLeft();
+          blueToothSerial.println("Turning on spot left.");
+          break;
+        case 'e':
+          turnOnSpotRight();
+          blueToothSerial.println("Turning on spot right.");
+          break;
+        case ' ':
+          stopAll();
+          blueToothSerial.println("Stopped.");
+          break;
+        case 'z':
+          reverseLeft();
+          blueToothSerial.println("Reversing left.");
+          break;
+        case 'c':
+          reverseRight();
+          blueToothSerial.println("Reversing right.");
+          break;
+        case 'p':
+          autopilot = true;
+          Serial.println("AUTO");
+          blueToothSerial.println("Autopilot enabled");
+          autopilotFn();
+          break;
+      }
+    }
+  }
+}
+
+void autopilotFn() {
+  
+  boolean parkRight = false;
+  boolean parkLeft = false;
+  boolean parked = false;
+
+  while (autopilot == true) {
+    analogWrite(ledPin, 255);
+    int sensorRight = analogRead(sensor2);
+    int sensorLeft = analogRead(sensor3);
+    int sensorRightFork = analogRead(sensor1);
+    int sensorLeftFork = analogRead(sensor4);
+    Serial.print("sensor1: ");
+    Serial.print(sensorRight);
+    Serial.print("   sensor2: ");
+    Serial.print(sensorLeft);
+    Serial.print("   sensor3: ");
+    Serial.print(sensorRightFork);
+    Serial.print("   sensor4: ");
+    Serial.println(sensorLeftFork);
+
+    //==== LINE FOLLOWING ALGORITHM ====
+    if (sensorLeft < leftBound && sensorRight < rightBound) { // i.e. Front 2 sensors sense white
+      goForward();
+    }
+    
+    if (sensorLeft > leftBound && sensorRight < rightBound) { // i.e. Front Left sensor senses Black, Front Right senses White
+      goLeft();
+      Serial.println("GOING LEFT");
+    }
+    
+    if (sensorLeft < leftBound && sensorRight > rightBound) { // i.e. Front Left sensor senses White, Front Right senses Black
+      goRight();
+      Serial.println("GOING RIGHT");
+    }
+
+    //==== FULLY PARKED ====
+    if(sensorLeft > leftBound && sensorRight > rightBound && parked == true) {
+      stopAll(); 
+    }
+
+    //==== RIGHT PARKING ALGORITHM ====
+    if (sensorRightFork < rightWall && parked == false) {
+      parkRight = true;
+      Serial.println("RIGHT WALL DETECTED");
+    }
+
+    if ((sensorRightFork > rightWall) && (parkRight == true)) {
+      stopAll();
+      blueToothSerial.println("Park Right y/n?");
+
+      while(1) {
+        if (blueToothSerial.available()) { //check if there's any data sent from the remote bluetooth shield
+          recvChar = blueToothSerial.read();
+          Serial.print(recvChar);
+          if (recvChar == 'y') {
+            stopAll();
+            delay(500);
+            goForward();
+            delay(1300);
+            stopAll();
+            delay(500);
+            turnOnSpotRight();
+            delay(850);
+            stopAll();
+            delay(500);
+            parkRight = false;
+            parked = true;
+            break;
+          } if (recvChar == 'n') {
+            parkRight = false;
+            parked = false;
+            break;
+          }
+        }
+      }
+    }
+
+    //==== LEFT PARKING ALGORITHM ====
+    if (sensorLeftFork < leftWall && parked == false) {
+      parkLeft = true;
+      Serial.println("LEFT WALL DETECTED");
+    }
+
+    if ((sensorLeftFork > leftWall) && (parkLeft == true)) {
+      stopAll();
+      blueToothSerial.println("Park Left y/n?");
+
+      while(1) {
+        if (blueToothSerial.available()) { //check if there's any data sent from the remote bluetooth shield
+          recvChar = blueToothSerial.read();
+          Serial.print(recvChar);
+          
+          if (recvChar == 'y') {
+            stopAll();
+            delay(500);
+            goForward();
+            delay(1300);
+            stopAll();
+            delay(500);
+            turnOnSpotLeft();
+            delay(850);
+            stopAll();
+            delay(500);
+            parkLeft = false;
+            parked = true;
+            break;
+          } if (recvChar == 'n') {
+            parkLeft = false;
+            parked = false;
+            break;
+          }
+        }
+      }
+    }
+
+    //==== STILL ON AUTOPILOT? ====
+    if (blueToothSerial.available()) {       //check if there's any data sent from the remote bluetooth shield
+      recvChar = blueToothSerial.read();
+      Serial.print(recvChar);
+      if (recvChar == 'p') {
+        autopilot = false;
+        Serial.println("AUTO OFF");
+        blueToothSerial.println("Autopilot disabled, resuming manual navigation.");
+      }
+    }
+    
+  }
+}
+
+void goForward() {
+  right.writeMicroseconds(speedRight);
+  left.writeMicroseconds(speedLeft);
+}
+
+void stopAll() {
+  right.writeMicroseconds(servoStop);
+  left.writeMicroseconds(servoStop);
+}
+
+void goLeft() {
+  left.writeMicroseconds(servoStop);
+  right.writeMicroseconds(speedRight);
+}
+
+void goRight() {
+  right.writeMicroseconds(servoStop);
+  left.writeMicroseconds(speedLeft);
+}
+
+void reverse() {
+  right.writeMicroseconds(speedLeft);
+  left.writeMicroseconds(speedRight);
+}
+
+void reverseLeft() {
+  left.writeMicroseconds(servoStop);
+  right.writeMicroseconds(revRight);
+}
+
+void reverseRight() {
+  right.writeMicroseconds(servoStop);
+  left.writeMicroseconds(revLeft);
+}
+
+void turnOnSpotLeft() {
+  right.writeMicroseconds(speedRight);
+  left.writeMicroseconds(revLeft);
+}
+
+void turnOnSpotRight() {
+  right.writeMicroseconds(revRight);
+  left.writeMicroseconds(speedLeft);
+}
+
+void setupBlueToothConnection()
+{
+  blueToothSerial.begin(38400);                           // Set BluetoothBee BaudRate to default baud rate 38500
+  blueToothSerial.print("\r\n+STWMOD=0\r\n");             // set the bluetooth work in slave mode
+  blueToothSerial.print("\r\n+STNA=SlaveUnique\r\n");    // set the bluetooth name as "SeeedBTSlave"
+  blueToothSerial.print("\r\n+STOAUT=1\r\n");             // Permit Paired device to connect me
+  blueToothSerial.print("\r\n+STAUTO=0\r\n");             // Auto-connection should be forbidden here
+  delay(2000);                                            // This delay is required.
+  while (blueToothSerial.available()) {
+    Serial.write( blueToothSerial.read());
+  }
+  blueToothSerial.print("\r\n+INQ=1\r\n");                // make the slave bluetooth inquirable
+  Serial.println("The slave bluetooth is inquirable!");
+  blueToothSerial.flush();
+  delay(2000);                                            // This delay is required.
+  while (blueToothSerial.available()) {
+    Serial.write( blueToothSerial.read());
+  }
+}
+
